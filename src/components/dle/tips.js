3daefef64fede1ee4dd8d2e2cf4d20ae42f0e514
Lucas Mastrovito <39515887+LucasMastrovito@users.noreply.github.com>
2025-10-26 23:37:29 +0100

    Update tips.js

--- src/components/dle/tips.js
+++ src/components/dle/tips.js
@@ -1,6 +1,6 @@
 function Tips(props) {
     return (
-        <div className={`card card-${props.game} scale tips-container`} style={{padding: "1em"}}>
+        <div className={`card card-${props.game} tips-container`} style={{padding: "1em"}}>
             <h3 className="outline" style={{marginTop: "0"}}>{props.name}</h3>
             { props.tries > 0 ?
                 <div>
