de36aa33a4ee12bc07379193dcb4d9e330549b49
Lucas Mastrovito <lucas.mastrovito@epitech.eu>
2025-11-05 17:03:02 +0100

    game dle style

--- src/pages/games.js
+++ src/pages/games.js
@@ -8,14 +8,12 @@ import Dle from "../components/dle/dle";
 
 function Games() {
     const buttons = [
-        <Dlemenubtn key={1} menu={true} name={"Classic"} desc={"Find the daily games"} logo="classic-games" index={1} />,
-        <Dlemenubtn key={2} menu={true} name={"Picture"} desc={"Guess from a filtered image"} logo="picture-games" index={2} />
+        <Dlemenubtn key={1} menu={true} name={"Classic"} desc={"Find the daily games"} logo="classic-games" index={1} />
     ];
 
     const modes = [
         <Dlemenu key={'menu'} title="Guess the daily games" name="games" menu={true} buttons={buttons} />,
-        <Classic key={'classic'} name={"games"} mode="classic" desc={"Find the daily games"} fields={fields} first_tips={{name: "Year", key: "year"}} second_tips={{name: "Developper", key: "developer"}} />,
-        <Findmode key={'picture'} name={"games"} mode="picture" desc={"Guess from a filtered image"} filter="image_url" />
+        <Classic key={'classic'} name={"games"} mode="classic" desc={"Find the daily games"} fields={fields} first_tips={{name: "Year", key: "year"}} second_tips={{name: "Developper", key: "developer"}} />
     ];
 
     return (
